ae9164e9b0d7c8cb47861bf055ec47d17092cc6c
Lorin <lrmt13@gmail.com>
2023-03-10 23:29:28 -0700

    initial commit

---
+++ login.js
@@ -0,0 +1,8 @@
+function login() {
+    const nameEl = document.querySelector("#name");
+    const emailEl = document.querySelector("#email");
+    localStorage.setItem("userName", nameEl.value);
+    localStorage.setItem("userEmail", emailEl.value);
+    window.location.href = "profile.html";
+  }
+  
