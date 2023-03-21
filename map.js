e8236d4222f93b59e1e047b7492a34fb345cf2c7
Lorin <lrmt13@gmail.com>
2023-03-21 13:58:51 -0600

    move function that load user info to profile.js

--- map.js
+++ map.js
@@ -3,23 +3,6 @@ class Map {
 
     constructor() { 
         this.statesSet = new Set();
-
-        const userNameEl = document.getElementsByClassName('user-name');
-        const userEmailEl = document.getElementsByClassName('user-email');
-        if (userNameEl.length > 0) {
-            userNameEl[0].textContent = this.getUserName();
-        }
-        if (userEmailEl.length > 0) {
-            userEmailEl[0].textContent = this.getUserEmail();
-        }
-    }
-
-    getUserName() {
-        return localStorage.getItem('userName') ?? 'Not provided';
-    }
-
-    getUserEmail() {
-        return localStorage.getItem('userEmail') ?? 'Not provided';
     }
     
     stateSelected(state) {
