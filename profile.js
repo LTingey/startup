85b6b01935c103738298f22719c87546f956fb9c
Lorin <lrmt13@gmail.com>
2023-03-21 13:59:45 -0600

    initial commit

---
+++ profile.js
@@ -0,0 +1,39 @@
+function loadList() {
+    let statesList = [];
+    statesText = localStorage.getItem('statesArray');
+    
+    if (statesText) {
+        statesList = JSON.parse(statesText);
+    }
+
+    const statesListEl = document.getElementById('states-list');
+
+    statesList.forEach(state => {
+        const newState = document.createElement('p');
+        newState.textContent = state;
+        statesListEl.appendChild(newState);
+    })
+}
+
+function loadProfile() {
+    const userNameEl = document.getElementsByClassName('user-name');
+    const userEmailEl = document.getElementsByClassName('user-email');
+        
+    if (userNameEl.length > 0) {
+        userNameEl[0].textContent = getUserName();
+    }
+    if (userEmailEl.length > 0) {
+        userEmailEl[0].textContent = getUserEmail();
+    }
+}
+
+function getUserName() {
+    return localStorage.getItem('userName') ?? 'Not provided';
+}
+
+function getUserEmail() {
+    return localStorage.getItem('userEmail') ?? 'Not provided';
+}
+
+loadProfile();
+loadList();
