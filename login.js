913102e38c550781653f779ba68e99bf7d856287
Lorin <lrmt13@gmail.com>
2023-03-23 20:56:02 -0600

    move files to public folder

--- login.js
+++
@@ -1,7 +0,0 @@
-function login() {
-  const nameEl = document.querySelector("#name");
-  const emailEl = document.querySelector("#email");
-  localStorage.setItem("userName", nameEl.value);
-  localStorage.setItem("userEmail", emailEl.value);
-  window.location.href = "map.html";
-}  
