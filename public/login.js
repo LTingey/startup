371faaa77d864c0d0a36813c465c3d9387977384
Lorin <lrmt13@gmail.com>
2023-04-05 07:41:33 -0600

    add newUser function with fetch request

--- public/login.js
+++ public/login.js
@@ -3,9 +3,27 @@ function login() {
   const emailEl = document.querySelector("#email");
   localStorage.setItem("userName", nameEl.value);
   localStorage.setItem("userEmail", emailEl.value);
+
   window.location.href = "map.html";
 }  
 
-function newUser() {
-  
+async function newUser() {
+  const nameEl = document.querySelector("#name");
+  const emailEl = document.querySelector("#email");
+  localStorage.setItem("userName", nameEl.value);
+  localStorage.setItem("userEmail", emailEl.value);
+
+  try {
+    const newList = {user: emailEl.value, states: []};
+    await fetch('/api/newList', {
+      method: 'POST',
+      headers: {'content-type': 'application/json'},
+      body: JSON.stringify(newList),
+    });
+  }
+  catch {
+    localStorage.setItem("statesList", []);
+  }
+
+  window.location.href = "map.html";
 }
