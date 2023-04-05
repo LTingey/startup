0b071b71555e3461132d712c37a41866cb264682
Lorin <lrmt13@gmail.com>
2023-04-05 15:52:46 -0600

    add endpoints for creating and authenticating accounts

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
+const cookieParser = require('cookie-parser');
+const bcrypt = require('bcrypt');
 const express = require('express');
 const app = express();
 const DB = require('./database.js');
+
+const authCookieName = 'token';
 
 // The service port. In production the application is statically hosted by the service on the same port.
 const port = process.argv.length > 2 ? process.argv[2] : 4000;
 
 // JSON body parsing using built-in middleware
 app.use(express.json());
+
+// Use the cookie parser middleware for tracking authentication tokens
+app.use(cookieParser());
 
 // Serve up the application's static content
 app.use(express.static('public'));
@@ -16,15 +23,57 @@ var apiRouter = express.Router();
 app.use(`/api`, apiRouter);
 
 
+// Create user
+apiRouter.post('/auth/create', async (req,res) => {
+  console.log("POST user request: ", req.body);
+  if (await DB.getUser(req.body.email)) {
+    console.log("Email is already being used");
+    res.status(409).send({msg: 'Existing user'})
+  }
+  else {
+    const user = await DB.createUser(req.body.email, req.body.name, req.body.password);
+    setAuthCookie(res, user.token);
+    res.send({id: user._id})
+  }
+})
+
+function setAuthCookie(res, authToken) {
+  res.cookie(authCookieName, authToken, {
+    secure: true,
+    httpOnly: true,
+    sameSite: 'strict',
+  });
+}
+
+// Check credentials
+apiRouter.post('/auth/login', async (req, res) => {
+  const user = await DB.getUser(req.body.email);
+  if (user) {
+    if (await bcrypt.compare(req.body.password, user.password)) {
+      setAuthCookie(res, user.token);
+      res.send({ id: user._id });
+      return;
+    }
+  }
+  res.status(401).send({ msg: 'Unauthorized' });
+});
+
+// DeleteAuth token if stored in cookie
+apiRouter.delete('/auth/logout', (_req, res) => {
+  res.clearCookie(authCookieName);
+  res.status(204).end();
+});
+
 // Create new states object
-apiRouter.post('/newList', async (req,res) => {
-  console.log("POST request: ", req.body);
-  DB.createList(req.body);
+apiRouter.post('/newList', async (req, res) => {
+  console.log("POST states request: ", req.body);
+  const result = await DB.createList(req.body);
+  res.send(result);
 })
 
 // Get states list
 apiRouter.get('/userList/:id', async (req, res) => {
-  console.log("GET");
+  console.log("GET states");
   const id = req.params.id;
   const result = await DB.getList(id);
   if (!result) {
@@ -35,9 +84,9 @@ apiRouter.get('/userList/:id', async (req, res) => {
 })
 
 // Update states list
-apiRouter.put('/updateList/:email', async (req, res) => {
-  console.log("PUT request: ", req.body);
-  DB.updateList(req.params.email, req.body);
+apiRouter.put('/updateList/:email', async (req) => {
+  console.log("PUT states request: ", req.body);
+  const result = await DB.updateList(req.params.email, req.body);
 })
 
 
