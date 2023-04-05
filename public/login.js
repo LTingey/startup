bf0d39cafc73ab2c1b81d97f332b2903ae913ed6
Lorin <lrmt13@gmail.com>
2023-04-05 15:51:55 -0600

    edit login() for user authentication

--- public/login.js
+++ public/login.js
@@ -1,11 +1,31 @@
-function login() {
+async function login() {
   const name = document.querySelector("#name")?.value;
   const email = document.querySelector("#email")?.value;
   const password = document.querySelector("#password")?.value;
   localStorage.setItem("userName", name);
   localStorage.setItem("userEmail", email);
 
-  window.location.href = "map.html";
+  try {
+    const result = await fetch('/api/auth/login', {
+      method: 'POST',
+      headers: {'content-type': 'application/json'},
+      body: JSON.stringify({email: email, password: password}),
+    });
+    if (result.ok) {
+      window.location.href = "map.html";              // where I left off, need to test to see if it works
+    }
+    else if (result.status === 409) {                 // up next is logout
+      throw new Error("Unauthorized");
+    }
+    else {
+      throw new Error("Error occured");
+    }
+  }
+  catch(error) {
+    const errorMessageEl = document.getElementById('errorMessage');
+    errorMessageEl.textContent = error.message;
+    errorMessageEl.style.display = 'block';
+  }
 }  
 
 async function newUser() {
