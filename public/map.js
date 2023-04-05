8487fe43bf048a21226f1767c5a44820d4ce86c1
Lorin <lrmt13@gmail.com>
2023-04-05 07:43:15 -0600

    add fetch requests to get and store states in db

--- public/map.js
+++ public/map.js
@@ -1,13 +1,39 @@
 let statesSet;
-if (localStorage.getItem('statesArray')) {
-    let statesArray = JSON.parse(localStorage.getItem('statesArray'));
+const email = localStorage.getItem('userEmail');
+
+// if (localStorage.getItem('statesArray')) {
+//     let statesArray = JSON.parse(localStorage.getItem('statesArray'));
+//     statesSet = new Set(statesArray);
+// }  
+// else {
+//     statesSet = new Set();
+// }
+
+async function loadSet() {
+    let statesArray;
+    try {
+        const statesObject = await fetch(`/api/userList/${email}`).then(response => response.json());
+        
+        if (statesObject) {
+            statesArray = statesObject.states;
+        }
+        else {
+            statesArray = new Set();
+        }
+
+        //localStorage.setItem('statesArray', JSON.stringify(statesArray));    // save list in case we go offline; do this later
+    }
+    catch {
+        const statesText = localStorage.getItem('statesArray')
+        if (statesText) {
+            statesSet = new Set(JSON.parse(statesText));
+        }
+    }
     statesSet = new Set(statesArray);
-}  
-else {
-    statesSet = new Set();
 }
 
-function loadMap() {
+async function loadMap() {
+    await loadSet();
     statesSet.forEach(state => {
         const fillStateEl = document.getElementById(state);
         fillStateEl.setAttribute('style', 'fill: #780707 !important');
@@ -37,9 +63,20 @@ function deselectState(state) {
     fillStateEl.setAttribute('style', 'fill: dimgrey !important');
 }
     
-function saveStates() {
-    let statesArray = Array.from(statesSet);
+async function saveStates() {
+    const statesArray = Array.from(statesSet);
     localStorage.setItem("statesArray", JSON.stringify(statesArray));
+
+    try {
+        await fetch(`/api/updateList/${email}`, {
+            method: 'PUT',
+            headers: { 'content-type': 'application/json' },
+            body: JSON.stringify(statesArray),
+        })
+    }
+    catch {
+        localStorage.setItem("statesArray", JSON.stringify(statesArray));
+    }
 }
 
 loadMap();
