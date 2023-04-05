9887abd9518b11d21af1932d627eef30c0ccb0d4
Lorin <lrmt13@gmail.com>
2023-04-05 13:26:55 -0600

    edit newUser to check email and create object in db

--- public/login.js
+++ public/login.js
@@ -1,29 +1,49 @@
 function login() {
-  const nameEl = document.querySelector("#name");
-  const emailEl = document.querySelector("#email");
-  localStorage.setItem("userName", nameEl.value);
-  localStorage.setItem("userEmail", emailEl.value);
+  const name = document.querySelector("#name")?.value;
+  const email = document.querySelector("#email")?.value;
+  const password = document.querySelector("#password")?.value;
+  localStorage.setItem("userName", name);
+  localStorage.setItem("userEmail", email);
 
   window.location.href = "map.html";
 }  
 
 async function newUser() {
-  const nameEl = document.querySelector("#name");
-  const emailEl = document.querySelector("#email");
-  localStorage.setItem("userName", nameEl.value);
-  localStorage.setItem("userEmail", emailEl.value);
+  const name = document.querySelector("#name")?.value;
+  const email = document.querySelector("#email")?.value;
+  const password = document.querySelector("#password")?.value;
+  localStorage.setItem("userName", name);
+  localStorage.setItem("userEmail", email);
 
   try {
-    const newList = {user: emailEl.value, states: []};
-    await fetch('/api/newList', {
+    const result = await fetch('/api/auth/create', {
       method: 'POST',
-      headers: {'content-type': 'application/json'},
-      body: JSON.stringify(newList),
+      headers: {'content-type': 'application/json; charset=UTF-8'},
+      body: JSON.stringify({name: name, email: email, password: password}),
     });
+    const body = await result.json();
+  
+    if (result.ok) {
+      const newList = {user: email, states: []};
+      const response = await fetch('/api/newList', {
+        method: 'POST',
+        headers: {'content-type': 'application/json'},
+        body: JSON.stringify(newList),
+      });
+      if (response.ok) {
+        window.location.href = "map.html";
+      }
+    }
+    else if (result.status === 409) {
+      throw new Error("Existing user");
+    }
+    else {
+      throw new Error("Error occured");
+    }
   }
-  catch {
-    localStorage.setItem("statesList", []);
+  catch(error) {
+    const errorMessageEl = document.getElementById('errorMessage');
+    errorMessageEl.textContent = error.message;
+    errorMessageEl.style.display = 'block';
   }
-
-  window.location.href = "map.html";
 }
